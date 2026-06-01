23bb9de47af34460b41a62bb7a08e9f0d4f3ad0b
misokatsu126 <126jun@gmail.com>
2026-06-02 02:06:01 +0900

    Serve Musubu root page directly

--- next.config.mjs
+++ next.config.mjs
@@ -9,16 +9,6 @@ const nextConfig = {
       }
     ];
   },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/",
-          destination: "/v1-clean"
-        }
-      ]
-    };
-  },
   experimental: {
     cpus: 1,
     parallelServerBuildTraces: false,
